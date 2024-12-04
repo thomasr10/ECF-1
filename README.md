6efcea09d9f0e131a3367801c4a36537cd91bb63
thomasr10 <t.rivoire01@gmail.com>
2024-12-04 16:39:04 +0100

    site presque fini

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ECF-1
+figma : https://www.figma.com/design/qbRPPUiBxvLvAN3gNUZPdt/ECF---1-(Site-vitrine-artiste-2D)?node-id=0-1&node-type=canvas&t=hpgCc4U4yJUiaCpo-0
--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,23 @@ burgerMenuButton.onclick = function(){
     const isOpen = burgerMenu.classList.contains('open');
     burgerMenuIcon.classList = isOpen ? 'fa-solid fa-x' : 'fa-solid fa-bars';
 }
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1,
+}
+
+const handleIntersect = function(entries, observer){
+    entries.forEach(function(entry){
+        if(entry.intersectionRatio > 0.1) {
+            entry.target.classList.add('reveal-visible');
+            observer.unobserve(entry.target)
+        };
+    })
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('.reveal').forEach(function(r){
+    observer.observe(r)
+});
